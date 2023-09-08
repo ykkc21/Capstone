7365db152ef1ae811292bc617a5afc3d957d1cab
ykkc21 <ykkc1595@gmail.com>
2023-09-08 21:46:44 +0900

    commit

--- project/src/App.js
+++ project/src/App.js
@@ -1,6 +1,12 @@
 import "../src/styles/App.css";
-
+import React, { useEffect } from "react";
 function App() {
+  useEffect(() => {
+    fetch("http://localhost:8080/api")
+      .then((res) => res.json)
+      .then((data) => console.log(data));
+  });
+
   return (
     <div>
       <h1>React Start</h1>
--- project/src/server.js
+++ project/src/server.js
@@ -2,3 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const server = require("http").createServer(app);
+
+//CORS 사용
+app.cors();
+
+//API로 요청한 경우 hello 메세지를 보내는 콜랙함수
+app.get("/api", (req, res) => {
+  res.send({ message: "hello" });
+});
+
+// 서버가 잘 동작하는지 확인
+server.listen(8080, () => {
+  console.log("server is running on 8080");
+});
