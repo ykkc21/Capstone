1e62595073a32e8bc5eef075f44ca15d802708ac
ykkc21 <ykkc1595@gmail.com>
2023-09-13 23:56:32 +0900

    commit
    commit

--- project/src/App.js
+++ project/src/App.js
@@ -42,6 +42,10 @@ function App() {
         </div>
       </header>
       <section className={style.View}>
+        <div className={style.ViewContainer}>
+          <h1 className={style.View_h1}>Information About Travel At Once</h1>
+          <div className={style.View_box}></div>
+        </div>
         <div className={style.View_Shadow}></div>
       </section>
     </Fragment>
