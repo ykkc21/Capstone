3b13b72ed7a78b2f15d07df531efb33f3761b281
ykkc21 <ykkc1595@gmail.com>
2023-09-13 23:29:23 +0900

    commit
    commit

--- project/src/App.js
+++ project/src/App.js
@@ -21,24 +21,24 @@ function App() {
           </div>
           <ul className={style.Header_ul}>
             <li className={style.Header_li}>
-              <a href="/" className={style.Header_a}>
+              <a href="/" className={style.Header_li_a}>
                 TraveList
               </a>
             </li>
             <li className={style.Header_li}>
-              <a href="/" className={style.Header_a}>
+              <a href="/" className={style.Header_li_a}>
                 Hotel
               </a>
             </li>
             <li className={style.Header_li}>
-              <a href="/" className={style.Header_a}>
+              <a href="/" className={style.Header_li_a}>
                 AirlineTicket
               </a>
             </li>
           </ul>
-          <a href="/" className={style.Header_Login}>
-            Sing in
-          </a>
+          <div className={style.Hedaer_Login}>
+            <button className={style.Hedaer_Login_btn}>Sing in</button>
+          </div>
         </div>
       </header>
       <section className={style.View}>
