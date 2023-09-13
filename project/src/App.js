629c9d2438179bed910861b91f84957d9835d0e3
ykkc21 <ykkc1595@gmail.com>
2023-09-13 23:16:36 +0900

    commit
    commit

--- project/src/App.js
+++ project/src/App.js
@@ -12,8 +12,38 @@ function App() {
 
   return (
     <Fragment>
-      <header className={style.Header}></header>
-      <section className={style.View}></section>
+      <header className={style.Header}>
+        <div className={style.Header_container}>
+          <div className={style.Header_Logo}>
+            <a className={style.Header_Logo_a} href="/">
+              LOGO
+            </a>
+          </div>
+          <ul className={style.Header_ul}>
+            <li className={style.Header_li}>
+              <a href="/" className={style.Header_a}>
+                TraveList
+              </a>
+            </li>
+            <li className={style.Header_li}>
+              <a href="/" className={style.Header_a}>
+                Hotel
+              </a>
+            </li>
+            <li className={style.Header_li}>
+              <a href="/" className={style.Header_a}>
+                AirlineTicket
+              </a>
+            </li>
+          </ul>
+          <a href="/" className={style.Header_Login}>
+            Sing in
+          </a>
+        </div>
+      </header>
+      <section className={style.View}>
+        <div className={style.View_Shadow}></div>
+      </section>
     </Fragment>
   );
 }
