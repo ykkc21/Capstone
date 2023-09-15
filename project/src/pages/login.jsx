5dad9298fc6b4d8eef4d6c77f42fcc2058ef6de0
ykkc21 <ykkc1595@gmail.com>
2023-09-15 23:25:14 +0900

    commit
    commit

--- project/src/pages/login.jsx
+++ project/src/pages/login.jsx
@@ -5,12 +5,31 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <Fragment>
+      <div className={style.login_View}></div>
       <div className={style.login_wrap}>
         <div className={style.login_container}>
           <div className={style.login_box}>
             <h1 className={style.login_box_h1}>
               <Link to={"/"}>LOGO</Link>
             </h1>
+            <p className={style.login_text}>Login</p>
+            <form>
+              <input
+                type="text"
+                className={style.login_input}
+                placeholder="User Id"
+              />
+              <input
+                type="text"
+                className={style.login_input}
+                placeholder="Password"
+              />
+              <input
+                type="submit"
+                value={"Sign up"}
+                className={style.login_input_submit}
+              />
+            </form>
           </div>
         </div>
       </div>
