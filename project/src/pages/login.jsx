c112cd438277847ae5b204726dacd4afc5dbd1c3
ykkc21 <ykkc1595@gmail.com>
2023-09-15 19:36:43 +0900

    commit
    commit

--- project/src/pages/login.jsx
+++ project/src/pages/login.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
 import style from "../styles/Login.module.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <h1>로그인 페이지 입니다.</h1>
-    </div>
+    <Fragment>
+      <div className={style.login_wrap}>
+        <div className={style.login_container}>
+          <div className={style.login_box}>
+            <h1 className={style.login_box_h1}>
+              <Link to={"/"}>LOGO</Link>
+            </h1>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
