3da2442703198ecafaa757950f3b5a4dcbbc9d20
ykkc21 <ykkc1595@gmail.com>
2023-09-16 21:29:37 +0900

    commit
    commit

--- project/src/pages/login.jsx
+++ project/src/pages/login.jsx
@@ -13,16 +13,18 @@ const Login = () => {
               <Link to={"/"}>LOGO</Link>
             </h1>
             <p className={style.login_text}>Login</p>
-            <form>
+            <form action="http://localhost:8080/loginData" method="POST">
               <input
                 type="text"
                 className={style.login_input}
                 placeholder="User Id"
+                name="user_id"
               />
               <input
                 type="text"
                 className={style.login_input}
                 placeholder="Password"
+                name="user_pw"
               />
               <button
                 type="submit"
@@ -32,11 +34,11 @@ const Login = () => {
                 Sign up
               </button>
             </form>
-            <div className={style.iconbox}>
+            {/* <div className={style.iconbox}>
               <a href="/" className={`${style.google} ${style.icon} `}></a>
               <a href="/" className={`${style.naver} ${style.icon}`}></a>
               <a href="/" className={`${style.facebook}  ${style.icon}`}></a>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
--- project/src/server.js
+++ project/src/server.js
@@ -5,13 +5,19 @@ const server = require("http").createServer(app);
 
 //CORS 사용
 app.use(cors());
+// 데이터를 json형식으로 파싱하겠다.
+app.use(express.json());
+//파싱하는 옵션지정 (false는 기본으로 내장된 querystring으로 받아온다.)
+app.use(express.urlencoded({ extended: false }));
 
 //API로 요청한 경우 hello 메세지를 보내는 콜랙함수
 app.get("/api", (req, res) => {
   res.json({ message: "hello" });
 });
 
-app.post("/login", (req, res) => {});
+app.post("/loginData", (req, res) => {
+  console.log(req.body);
+});
 
 // 서버가 잘 동작하는지 확인
 server.listen(8080, () => {
