afeba50ff44e834ff1b3ef369efd4412e149e885
ykkc21 <ykkc1595@gmail.com>
2023-09-19 21:28:36 +0900

    commit
    commit

--- project/src/constants/About.jsx
+++ project/src/constants/About.jsx
@@ -1,5 +1,7 @@
 import React, { Fragment } from "react";
 import style from "../styles/About.module.css";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const About = () => {
   return (
     <Fragment>
@@ -7,7 +9,15 @@ const About = () => {
         <div className={style.AboutContainer}>
           <h1>Where do you want to go? </h1>
           <div className={style.ItemBox}>
-            <div className={`${style.Item}`}></div>
+            <div className={`${style.Item}`}>
+              <div className={style.Item_TextBox}>
+                <FontAwesomeIcon
+                  icon={faLocationDot}
+                  className={style.Item_icon}
+                ></FontAwesomeIcon>
+                <span className={style.text}></span>
+              </div>
+            </div>
             <div className={`${style.Item}`}></div>
             <div className={`${style.Item}`}></div>
             <div className={`${style.Item}`}></div>
