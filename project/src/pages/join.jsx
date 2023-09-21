0264edb1672bfd9e40ed5d17dc0092262268bec5
ykkc21 <ykkc1595@gmail.com>
2023-09-21 17:43:07 +0900

    commit
    commit

---
+++ project/src/pages/join.jsx
@@ -0,0 +1,11 @@
+import { React, Fragment } from "react";
+import style from "../styles/Join.module.css";
+
+const Join = () => {
+  return (
+    <Fragment>
+      <div></div>
+    </Fragment>
+  );
+};
+export default Join;
