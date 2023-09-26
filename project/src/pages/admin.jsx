453c9b4af262d35729330c774194ecc112ade37c
ykkc21 <ykkc1595@gmail.com>
2023-09-26 20:55:10 +0900

    commit
    commit

--- project/src/pages/admin.jsx
+++ project/src/pages/admin.jsx
@@ -9,11 +9,27 @@ const admin = () => {
         <div className={style.CountBox}>
           <h1>Content</h1>
           <div className={style.s_countbox}>
+            <div className={`${style.s_box} ${style.first}`}></div>
             <div className={style.s_box}></div>
             <div className={style.s_box}></div>
-            <div className={style.s_box}></div>
-            <div className={style.s_box}></div>
+            <div className={`${style.s_box} ${style.last}`}></div>
           </div>
+        </div>
+
+        <div className={style.ContentListBox}>
+          <form action="/" method="get" className={style.form}>
+            <input
+              type="search"
+              name="search"
+              className={style.search}
+              placeholder="Searching...."
+            />
+            <select name="menu" className={style.menu}>
+              <option value="게시글">게시글</option>
+              <option value="관광지">관광지</option>
+              <option value="유저">유저</option>
+            </select>
+          </form>
         </div>
       </div>
     </Fragment>
