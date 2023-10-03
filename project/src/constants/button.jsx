b3a9a0cc9d20b0f6f5f49f94373352aaa411f6fd
ykkc21 <ykkc1595@gmail.com>
2023-10-03 17:21:17 +0900

    commit
    commit

--- project/src/constants/button.jsx
+++ project/src/constants/button.jsx
@@ -1,9 +1,14 @@
 import React, { Fragment } from "react";
 import style from "../styles/button.module.css";
 const Button = (props) => {
+  const Check = (e) => {
+    console.log();
+  };
   return (
     <Fragment>
-      <div className={style.button}>{(document.write = props.name)}</div>
+      <div className={`${style.button} ${props.name}`} onClick={Check}>
+        {(document.write = props.name)}
+      </div>
     </Fragment>
   );
 };
