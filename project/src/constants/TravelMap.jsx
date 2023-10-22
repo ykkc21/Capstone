e071ecbe9801b3ac6d23c1d19dafd7f477c746e6
ykkc21 <ykkc1595@gmail.com>
2023-10-22 21:57:35 +0900

    commit
    commit

---
+++ project/src/constants/TravelMap.jsx
@@ -0,0 +1,7 @@
+import { React, Fragment } from "react";
+import style from "../styles/TravelMap.module.css";
+
+const TravelMap = () => {
+  return <Fragment></Fragment>;
+};
+export default TravelMap;
--- project/src/pages/TravelList.jsx
+++ project/src/pages/TravelList.jsx
@@ -2,12 +2,14 @@ import { React, Fragment } from "react";
 import style from "../styles/TraveList.module.css";
 import Header from "../components/header";
 import TravelSideBar from "../constants/TravelSideBar";
+import TravelMap from "../constants/TravelMap";
 const TravelList = ({ userData }) => {
   return (
     <Fragment>
       <Header userData={userData} />
       <div className={style.warp}>
         <TravelSideBar />
+        <TravelMap />
       </div>
     </Fragment>
   );
