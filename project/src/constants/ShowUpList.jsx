10645bbc57dac8cf0854fd80b44ea11a37c399f4
ykkc21 <ykkc1595@gmail.com>
2023-10-24 17:19:26 +0900

    commit
    commit

--- project/src/constants/ShowUpList.jsx
+++ project/src/constants/ShowUpList.jsx
@@ -1,6 +1,23 @@
 import { React, Fragment } from "react";
 import style from "../styles/ShowUpList.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
-const ShowUpList = () => {};
+const ShowUpList = () => {
+  const TestClick = () => {
+    console.log(132);
+  };
+  return (
+    <Fragment>
+      <div className={style.SHowUpMain}>
+        <FontAwesomeIcon
+          icon={faChevronUp}
+          onClick={TestClick}
+          className={style.iconUp}
+        />
+      </div>
+    </Fragment>
+  );
+};
 
 export default ShowUpList;
--- project/src/constants/TravelMap.jsx
+++ project/src/constants/TravelMap.jsx
@@ -7,8 +7,8 @@ const TravelMap = () => {
     <Fragment>
       <div className={style.MainMap}>
         <img src={WorldMap} alt="worldmap" />
+        <ShowUpList />
       </div>
-      <ShowUpList />
     </Fragment>
   );
 };
