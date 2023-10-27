7cedf140920e9a5254bd67b1aa5664bdfb91862f
ykkc21 <ykkc1595@gmail.com>
2023-10-27 19:15:14 +0900

    commit
    commit

--- project/src/pages/TravelList.jsx
+++ project/src/pages/TravelList.jsx
@@ -1,16 +1,24 @@
-import { React, Fragment } from "react";
+import { React, Fragment, useEffect, useState } from "react";
 import style from "../styles/TraveList.module.css";
 import Header from "../components/header";
 import TravelSideBar from "../constants/TravelSideBar";
 import TravelMap from "../constants/TravelMap";
 const TravelList = ({ userData }) => {
+  const [animater, setAnimater] = useState(true);
+  useEffect(() => {}, []);
   return (
     <Fragment>
-      <Header userData={userData} />
-      <div className={style.warp}>
-        <TravelSideBar />
-        <TravelMap />
-      </div>
+      {animater ? (
+        <div>test</div>
+      ) : (
+        <>
+          <Header userData={userData} />
+          <div className={style.warp}>
+            <TravelSideBar />
+            <TravelMap />
+          </div>
+        </>
+      )}
     </Fragment>
   );
 };
