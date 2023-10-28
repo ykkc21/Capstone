beda2ee8512bcc4684785a170761b0be1f5cd95c
ykkc21 <ykkc1595@gmail.com>
2023-10-28 13:36:05 +0900

    commit
    commit

--- project/src/constants/Animater.jsx
+++ project/src/constants/Animater.jsx
@@ -1,6 +1,25 @@
 import { React, Fragment } from "react";
 import style from "../styles/Animater.module.css";
+import airplane from "../assets/img/airplane.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAnglesDown } from "@fortawesome/free-solid-svg-icons";
 const Animater = () => {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <div className={style.BackGround}>
+        <div className={style.airplane}>
+          <img src={airplane} alt="img" />
+        </div>
+        <h2>Getting Started with Travel</h2>
+        <div className={style.IconBox}>
+          <FontAwesomeIcon
+            id="first"
+            className={style.arrow}
+            icon={faAnglesDown}
+          />
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 export default Animater;
--- project/src/pages/TravelList.jsx
+++ project/src/pages/TravelList.jsx
@@ -3,13 +3,14 @@ import style from "../styles/TraveList.module.css";
 import Header from "../components/header";
 import TravelSideBar from "../constants/TravelSideBar";
 import TravelMap from "../constants/TravelMap";
+import Animater from "../constants/Animater";
 const TravelList = ({ userData }) => {
   const [animater, setAnimater] = useState(true);
   useEffect(() => {}, []);
   return (
     <Fragment>
       {animater ? (
-        <div>test</div>
+        <Animater />
       ) : (
         <>
           <Header userData={userData} />
