9e0de5e4085aad7e565bbc8ef06e1734af44537d
ykkc21 <ykkc1595@gmail.com>
2023-10-28 22:38:03 +0900

    commit
    commit

--- project/src/constants/ItemList.jsx
+++ project/src/constants/ItemList.jsx
@@ -1,8 +1,11 @@
 import React, { Fragment, useEffect } from "react";
 import style from "../styles/ItemList.module.css";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser } from "@fortawesome/free-regular-svg-icons";
 
 const ItemList = (props) => {
+  console.log(props);
   const UserDelete = (user) => {
     console.log(user);
     const test = axios
@@ -24,6 +27,14 @@ const ItemList = (props) => {
   return (
     <Fragment>
       <div className={style.itemlist}>
+        <div className={style.IconBox}>
+          <FontAwesomeIcon icon={faCircleUser} />
+        </div>
+        <div className={style.InfoBox}>
+          <h2>{props.name.toUpperCase()}</h2>
+          <h5>{props.email.toUpperCase()}</h5>
+        </div>
+        <div className={style.DeleteBox}></div>
         {/* <table>
           <tbody>
             {props.state == "User" ? (
