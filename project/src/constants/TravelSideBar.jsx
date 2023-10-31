c724348133d2ac28b9fbcf730d95c39b7b9f5950
ykkc21 <ykkc1595@gmail.com>
2023-11-01 00:16:19 +0900

    commit
    commit

--- project/src/constants/TravelSideBar.jsx
+++ project/src/constants/TravelSideBar.jsx
@@ -1,21 +1,85 @@
-import { React, Fragment } from "react";
+import { React, Fragment, useEffect, useState } from "react";
 import style from "../styles/TravelSideBar.module.css";
-const TravelSideBar = () => {
+const TravelSideBar = ({ ContentData }) => {
+  const [NCount, setNCount] = useState(0);
+  const [ACount, setACount] = useState(0);
+  const [ECount, setECount] = useState(0);
+  const [NLenght, setNLenght] = useState(0);
+  const [ALenght, setALenght] = useState(0);
+  const [ELenght, setELenght] = useState(0);
+
+  useEffect(() => {
+    const DataLength = () => {
+      const arrayData = ContentData.reduce((acc, curr) => acc.concat(curr), []);
+      console.log(arrayData);
+      const asiaCount = arrayData.filter(
+        (item) => item.c_classinfo === "Asia"
+      ).length;
+      const northAmericaCount = arrayData.filter(
+        (item) => item.c_classinfo === "NorthAmerica"
+      ).length;
+      const europeCount = arrayData.filter(
+        (item) => item.c_classinfo === "Europe"
+      ).length;
+      console.log(asiaCount, northAmericaCount, europeCount);
+      setNLenght(northAmericaCount);
+      setALenght(asiaCount);
+      setELenght(europeCount);
+    };
+    DataLength();
+
+    const A_Contnet = setInterval(() => {
+      if (ACount < ALenght) {
+        setACount((data) => data + 1); // 1씩 증가
+        console.log(ACount); // count 출력
+      } else {
+        clearInterval(A_Contnet); // 목표에 도달하면 interval 종료
+        console.log("목표에 도달했습니다!");
+      }
+    }, 250);
+
+    const N_Contnet = setInterval(() => {
+      if (NCount < NLenght) {
+        setNCount((data) => data + 1); // 1씩 증가
+        console.log(NCount); // count 출력
+      } else {
+        clearInterval(N_Contnet); // 목표에 도달하면 interval 종료
+        console.log("목표에 도달했습니다!");
+      }
+    }, 250);
+
+    const E_Contnet = setInterval(() => {
+      if (ECount < ELenght) {
+        setECount((data) => data + 1); // 1씩 증가
+        console.log(ECount); // count 출력
+      } else {
+        clearInterval(E_Contnet); // 목표에 도달하면 interval 종료
+        console.log("목표에 도달했습니다!");
+      }
+    }, 250);
+
+    return () => {
+      clearInterval(A_Contnet); // 컴포넌트가 언마운트될 때 interval 정리
+      clearInterval(N_Contnet); // 컴포넌트가 언마운트될 때 interval 정리
+      clearInterval(E_Contnet); // 컴포넌트가 언마운트될 때 interval 정리
+    };
+  }, [ACount, ALenght, NCount, NLenght, ECount, ELenght]);
+
   return (
     <Fragment>
       <div className={style.SideBarWarp}>
         <h1>TravelDetination</h1>
         <div className={style.NorthAmerica}>
           <h1>NorthAmerica</h1>
-          <h2>17</h2>
+          <h2>{NCount}</h2>
         </div>
         <div className={style.Asia}>
           <h1>Asia</h1>
-          <h2>10</h2>
+          <h2>{ACount}</h2>
         </div>
         <div className={style.Europe}>
           <h1>Europe</h1>
-          <h2>8</h2>
+          <h2>{ECount}</h2>
         </div>
       </div>
     </Fragment>
--- project/src/pages/TravelList.jsx
+++ project/src/pages/TravelList.jsx
@@ -8,44 +8,19 @@ import axios from "axios";
 const TravelList = ({ userData }) => {
   const [animater, setAnimater] = useState(true);
   const [array, setArray] = useState([]);
-  const [NLenght, setNLenght] = useState(0);
-  const [ALenght, setALenght] = useState(0);
-  const [ELenght, setELenght] = useState(0);
 
   useEffect(() => {
     const feachData = async () => {
       const contentsResponse = await axios.get("/Contents");
       if (contentsResponse.data.msg === "OK") {
-        setArray([...array, contentsResponse.data.contents]);
         setTimeout(() => {
           setAnimater(false);
+          setArray([...array, contentsResponse.data.contents]);
         }, 5000);
       }
     };
-
-    const DataLength = () => {
-      const ContentData = array.reduce((acc, curr) => acc.concat(curr), []);
-      console.log(ContentData);
-      const asiaCount = ContentData.filter(
-        (item) => item.c_classinfo === "Asia"
-      ).length;
-      const northAmericaCount = ContentData.filter(
-        (item) => item.c_classinfo === "NorthAmerica"
-      ).length;
-      const europeCount = ContentData.filter(
-        (item) => item.c_classinfo === "Europe"
-      ).length;
-
-      setNLenght((count) => count + northAmericaCount);
-      setALenght((count) => count + asiaCount);
-      setELenght((count) => count + europeCount);
-    };
-
     feachData();
-    DataLength();
   }, []);
-
-  console.log(NLenght, ALenght, ELenght);
 
   return (
     <Fragment>
@@ -55,7 +30,7 @@ const TravelList = ({ userData }) => {
         <>
           <Header userData={userData} />
           <div className={style.warp}>
-            <TravelSideBar />
+            <TravelSideBar ContentData={array} />
             <TravelMap />
           </div>
         </>
