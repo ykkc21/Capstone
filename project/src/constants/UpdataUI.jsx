17cc6ce7cd534af40d1c7c172be061b26f4f8c60
ykkc21 <ykkc1595@gmail.com>
2023-11-01 14:23:51 +0900

    commit
    commit

--- project/src/constants/UpdataUI.jsx
+++ project/src/constants/UpdataUI.jsx
@@ -30,6 +30,83 @@ const UpdateUI = ({ ContentId, CloseUpdateUI }) => {
     };
     SelectContent();
   }, []);
+
+  const getYoutubeId = (e) => {
+    const IdListbox = document.getElementById("IdList");
+    // const BtnUpload = document.getElementById("UploadBtn");
+    if (e.code === "Enter") {
+      const newArray = [...array, youtubeId];
+      const FilterArray = [...new Set(newArray)];
+      //중복 제거된 배열상태를 저장
+      setArray(FilterArray);
+
+      setYoutubeId("");
+
+      IdListbox.style.top = "65%";
+      IdListbox.style.opacity = 1;
+      IdListbox.style.transition = "0.5s";
+      IdListbox.style.zIndex = 10;
+    }
+  };
+
+  const deleteData = (idx) => {
+    const IdListbox = document.getElementById("IdList");
+    // const BtnUpload = document.getElementById("UploadBtn");
+    const updateArray = array.filter((_, index) => index !== idx);
+    setArray(updateArray);
+    if (array.length === 1) {
+      IdListbox.style.top = "58%";
+      IdListbox.style.opacity = 0;
+      IdListbox.style.transition = "0.5s";
+      IdListbox.style.zIndex = -1;
+    }
+  };
+
+  const ClickUpdate = async () => {
+    console.log("Click");
+    //   if (
+    //     (classinfo === "" ||
+    //       classinfo === null ||
+    //       title === "" ||
+    //       information === "" ||
+    //       location === "" ||
+    //       array.length === 0,
+    //     selectFile == null)
+    //   ) {
+    //     alert("데이터를 정확하게 작성해주세요!");
+    //   } else {
+    //     if (selectFile) {
+    //       const formData = new FormData();
+    //       formData.append("classinfo", classinfo);
+    //       formData.append("title", title);
+    //       formData.append("information", information);
+    //       formData.append("location", location);
+    //       formData.append("array", JSON.stringify(array));
+    //       formData.append("file", selectFile); // "file"은 서버에서 받을 파일 필드 이름
+    //       const uploadData = await axios.post(
+    //         "http://localhost:9000/Content/AddContent",
+    //         formData,
+    //         { headers: { "Content-Type": "multipart/form-data" } }
+    //       );
+    //       if (uploadData.data.msg === "OK") {
+    //         alert("업로드 완료했습니다.");
+    //         CloseBox();
+    //       } else {
+    //         alert("콘텐츠 업로드 실패");
+    //         AddContentBox();
+    //       }
+    //     }
+    //   }
+  };
+
+  const ChangeFile = (target) => {
+    setSelectFile(target.files[0]);
+  };
+
+  const FileStart = () => {
+    const dom = document.getElementById("InputFile");
+    dom.click();
+  };
   return (
     <Fragment>
       <div id="UpdateBox" className={style.UpdateBox}>
@@ -88,14 +165,18 @@ const UpdateUI = ({ ContentId, CloseUpdateUI }) => {
                     }
                   }
                 }}
-                // onKeyUp={getYoutubeId}
+                onKeyUp={getYoutubeId}
                 name="YoutubeId"
                 id="YoutubeId"
               />
-              <div id="IdList" className={style.Idlist}>
+              <div id="UpdateIdlist" className={style.UpdateIdlist}>
                 {array.map((item, idx) => {
                   return (
-                    <div key={idx} idx={idx} className={style.Idlist_item}>
+                    <div
+                      key={idx}
+                      idx={idx}
+                      className={style.update_Idlist_item}
+                    >
                       <FontAwesomeIcon
                         className={style.youtubeIcon}
                         icon={faYoutube}
@@ -103,7 +184,7 @@ const UpdateUI = ({ ContentId, CloseUpdateUI }) => {
                       {item}
                       <div
                         id="deleteBtn"
-                        // onClick={() => deleteData(idx)}
+                        onClick={() => deleteData(idx)}
                         idx={idx}
                         className={style.delete}
                       >
@@ -113,8 +194,7 @@ const UpdateUI = ({ ContentId, CloseUpdateUI }) => {
                   );
                 })}
               </div>
-              <div className={style.Files}>
-                {/* onClick={FileStart} */}
+              <div className={style.Files} onClick={FileStart}>
                 <div className={style.Circle}>
                   <FontAwesomeIcon
                     className={style.CircleIcon}
@@ -124,17 +204,17 @@ const UpdateUI = ({ ContentId, CloseUpdateUI }) => {
                 <button className={style.Btn}>Upload</button>
               </div>
               <input
-                // onChange={(e) => {
-                //   ChangeFile(e.target);
-                // }}
+                onChange={(e) => {
+                  ChangeFile(e.target);
+                }}
                 id="InputFile"
                 type="file"
                 name="Image"
               />
               <button
-                id="UploadBtn"
-                className={style.UploadBtn}
-                // onClick={ClickUpload}
+                id="UpdateBtn"
+                className={style.UpdateBtn}
+                onClick={ClickUpdate}
               >
                 Update
               </button>
