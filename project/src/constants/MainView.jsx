069b80064eaf4ae849f2e8e91c78c484d4d17526
ykkc21 <ykkc1595@gmail.com>
2023-11-03 00:02:11 +0900

    commit
    commit

---
+++ project/src/constants/MainView.jsx
@@ -0,0 +1,12 @@
+import { React, Fragment } from "react";
+import style from "../styles/MainView.module.css";
+
+const MainView = () => {
+  return (
+    <Fragment>
+      <div className={style.MainViewBox}></div>
+    </Fragment>
+  );
+};
+
+export default MainView;
--- project/src/pages/Home.jsx
+++ project/src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import { React, Fragment } from "react";
 import Hedaer from "../components/header";
+import MainView from "../constants/MainView";
 import Footer from "../components/footer";
+
 const Home = ({ userData }) => {
+  const style = {
+    width: "100%",
+    height: "100%",
+    display: "block",
+  };
   return (
-    <Fragment>
+    <div style={style}>
       <Hedaer userData={userData} />
-    </Fragment>
+      <MainView />
+    </div>
   );
 };
 
