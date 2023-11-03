2f6be9ba5bf45da61cbbb215d6bd4eb6c3ed7872
ykkc21 <ykkc1595@gmail.com>
2023-11-03 14:58:41 +0900

    commit
    commit

--- project/src/constants/MainView.jsx
+++ project/src/constants/MainView.jsx
@@ -1,7 +1,23 @@
-import { React, Fragment } from "react";
+import { React, Fragment, useState } from "react";
 import style from "../styles/MainView.module.css";
 
 const MainView = () => {
+  const [viewtext, setViewText] = useState("a happy trip");
+
+  const ChangText = (target) => {
+    const attr = target.getAttribute("for");
+    if (attr === "btn1") {
+      setViewText("a trip to Australia");
+    } else if (attr === "btn2") {
+      setViewText("a trip to London");
+    } else if (attr === "btn3") {
+      setViewText("a trip to Japan");
+    } else if (attr === "btn4") {
+      setViewText("a trip to Paris");
+    } else if (attr === "btn5") {
+      setViewText("a trip to Thailand");
+    }
+  };
   return (
     <Fragment>
       <input
@@ -35,22 +51,53 @@ const MainView = () => {
         id="btn5"
       />
       <div className={style.MainViewBox}>
-        <div className={style.container}>
-          <div className={style.SlideBox}>
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <div className={style.SlideBtnBox}>
-                <label htmlFor="btn1"></label>
-                <label htmlFor="btn2"></label>
-                <label htmlFor="btn3"></label>
-                <label htmlFor="btn4"></label>
-                <label htmlFor="btn5"></label>
-              </div>
-            </ul>
+        <div className={style.BackColor}>
+          <div className={style.container}>
+            <div className={style.SlideBox}>
+              <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <h1>
+                  Let's prepare for
+                  <br /> - {viewtext}
+                </h1>
+                <div className={style.SlideBtnBox}>
+                  <label
+                    onClick={(e) => {
+                      ChangText(e.target);
+                    }}
+                    htmlFor="btn1"
+                  ></label>
+                  <label
+                    onClick={(e) => {
+                      ChangText(e.target);
+                    }}
+                    htmlFor="btn2"
+                  ></label>
+                  <label
+                    onClick={(e) => {
+                      ChangText(e.target);
+                    }}
+                    htmlFor="btn3"
+                  ></label>
+                  <label
+                    onClick={(e) => {
+                      ChangText(e.target);
+                    }}
+                    htmlFor="btn4"
+                  ></label>
+                  <label
+                    onClick={(e) => {
+                      ChangText(e.target);
+                    }}
+                    htmlFor="btn5"
+                  ></label>
+                </div>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
