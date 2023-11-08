a85bb4670d0f5dd3766cdafdec150fec7ba12cd3
ykkc21 <ykkc1595@gmail.com>
2023-11-08 15:40:45 +0900

    commit
    commit

--- project/src/constants/TravelItemCard.jsx
+++ project/src/constants/TravelItemCard.jsx
@@ -4,6 +4,13 @@ import style from "../styles/TravelItemCard.module.css";
 import IconImage from "../assets/img/pack.png";
 const TravelItemCard = ({ elementData }) => {
   console.log(elementData);
+
+  const truncateText = (text, maxLength) => {
+    if (text.length <= maxLength) {
+      return text;
+    }
+    return text.slice(0, maxLength) + "...";
+  };
   return (
     <Fragment>
       <Link to={`/listdetail/${elementData.c_idx}`}>
@@ -12,7 +19,7 @@ const TravelItemCard = ({ elementData }) => {
           <div className={style.CardTextBox}>
             <img src={IconImage} alt="IconImage" />
             <h2>{elementData.c_title}</h2>
-            <p>{elementData.c_information}</p>
+            <p>{truncateText(elementData.c_information, 20)}</p>
           </div>
         </div>
       </Link>
