94180598f80e2e725664b3568292f020376ae4ae
ykkc21 <ykkc1595@gmail.com>
2023-11-13 21:12:44 +0900

    commit
    commit

--- project/src/constants/HotelList.jsx
+++ project/src/constants/HotelList.jsx
@@ -1,11 +1,18 @@
 import { React, Fragment } from "react";
+import { Link } from "react-router-dom";
 import style from "../styles/HotelList.module.css";
 
 const HotelList = ({ data }) => {
   console.log(data);
   return (
     <Fragment>
-      <div className={style.HotelListItem}></div>
+      <Link to={data.h_url}>
+        <div className={style.HotelListItem}>
+          <div className={style.BackImage}>
+            <img src={data.h_image} alt="listImage" />
+          </div>
+        </div>
+      </Link>
     </Fragment>
   );
 };
