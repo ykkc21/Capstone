4e33b70958afcb520cc6124e97054229297654e8
ykkc21 <ykkc1595@gmail.com>
2023-11-18 18:57:50 +0900

    commit
    commit

--- project/src/pages/AirLineInForMation.jsx
+++ project/src/pages/AirLineInForMation.jsx
@@ -1,8 +1,11 @@
-import { React, Fragment } from "react";
+import { React, Fragment, useEffect } from "react";
 import style from "../styles/AirlineTicket.module.css";
 import Header from "../components/header";
+import axios from "axios";
 
 const AirLineInForMation = ({ userData }) => {
+  useEffect(async () => {}, []);
+
   return (
     <Fragment>
       <Header userData={userData} />
