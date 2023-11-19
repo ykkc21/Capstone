5c4662388e508ee092a8cb2d87867ef36015bee5
ykkc21 <ykkc1595@gmail.com>
2023-11-19 15:15:46 +0900

    commit
    commit

---
+++ project/src/constants/AirLinePlate.jsx
@@ -0,0 +1,8 @@
+import { React, Fragment } from "react";
+import style from "../stylesAirLinePlate.module.css";
+
+const AirLinePlate = ({}) => {
+  return <Fragment></Fragment>;
+};
+
+export default AirLinePlate;
--- project/src/pages/AirLineInForMation.jsx
+++ project/src/pages/AirLineInForMation.jsx
@@ -1,6 +1,8 @@
 import { React, Fragment, useEffect } from "react";
 import style from "../styles/AirlineTicket.module.css";
 import Header from "../components/header";
+import AirLineView from "../constants/AirLineView";
+import AirLinePlate from "../constants/AirLinePlate";
 import axios from "axios";
 
 const AirLineInForMation = ({ userData }) => {
