7306757d4ae6fb5bc02ffc7fd3f9d04028131cd6
ykkc21 <ykkc1595@gmail.com>
2023-11-21 00:32:55 +0900

    commit
    commit

--- project/src/constants/AirLineList.jsx
+++ project/src/constants/AirLineList.jsx
@@ -1,7 +1,12 @@
 import { React, Fragment } from "react";
 import style from "../styles/AirLineList.module.css";
 
-const AirLineList = ({}) => {
-  return <Fragment></Fragment>;
+const AirLineList = ({ airdata }) => {
+  console.log(airdata);
+  return (
+    <Fragment>
+      <div className={style.AirListBox}></div>
+    </Fragment>
+  );
 };
 export default AirLineList;
--- project/src/constants/AirLinePlate.jsx
+++ project/src/constants/AirLinePlate.jsx
@@ -3,10 +3,22 @@ import style from "../styles/AirLinePlate.module.css";
 import AirLineList from "../constants/AirLineList";
 
 const AirLinePlate = ({ DataList }) => {
-  console.log(DataList);
+  const test = () => {
+    if (DataList.length == 0 || DataList.length < 0) {
+      return <h1>해당 데이터가 존자하지 않습니다.</h1>;
+    } else {
+      DataList.map((item, idx) => {
+        return <AirLineList key={idx} airdata={item} />;
+      });
+    }
+  };
+
+  test();
   return (
     <Fragment>
-      <div className={style.AirLinePlateBox}></div>
+      <div className={style.AirLinePlateBox}>
+        <div className={style.container}></div>
+      </div>
     </Fragment>
   );
 };
