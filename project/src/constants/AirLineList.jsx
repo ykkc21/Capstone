16df163a9eb29b129c629f06186a8195fc8017cd
ykkc21 <ykkc1595@gmail.com>
2023-11-21 23:17:03 +0900

    commit
    commit

--- project/src/constants/AirLineList.jsx
+++ project/src/constants/AirLineList.jsx
@@ -1,7 +1,8 @@
 import { React, Fragment } from "react";
 import style from "../styles/AirLineList.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBarcode } from "@fortawesome/free-solid-svg-icons";
+import { faBarcode, faPlane } from "@fortawesome/free-solid-svg-icons";
+import QRCode from "qrcode.react";
 const AirLineList = ({ airdata }) => {
   console.log(airdata);
   return (
@@ -10,11 +11,50 @@ const AirLineList = ({ airdata }) => {
         <div className={style.backBox}>
           <h3>출발정보</h3>
           <div className={style.airportInfoBox}>
-            <div className={style.firstbox}></div>
-            <div className={style.middlebox}></div>
-            <div className={style.lastbox}></div>
+            <div className={style.firstbox}>
+              <h3 className={style.firstTitle}>
+                인천공항
+                <br />
+                (ICN)
+              </h3>
+              <FontAwesomeIcon className={style.AirportIcon} icon={faPlane} />
+              <h3 className={style.lastTitle}>
+                {airdata.airport}
+                <br />({airdata.airportCode})
+              </h3>
+            </div>
+            <div className={style.middlebox}>
+              <div className={style.infobox}>
+                <h4>항공사</h4>
+                <p>{airdata.airline}</p>
+              </div>
+              <div className={style.infobox}>
+                <h4>출발 예정시간</h4>
+                <p>{airdata.scheduleDateTime}</p>
+              </div>
+              <div className={style.infobox}>
+                <h4>출발 변경시간</h4>
+                <p>{airdata.estimatedDateTime}</p>
+              </div>
+              <div className={style.infobox}>
+                <h4>편명</h4>
+                <p>{airdata.flightId}</p>
+              </div>
+            </div>
+            <div className={style.lastbox}>
+              <div className={style.subbox}>
+                <h4>탑승구</h4>
+                <p>{airdata.gatenumber}</p>
+              </div>
+              <div className={style.subbox}>
+                <h4>터미널</h4>
+                <p>{airdata.terminalId}</p>
+              </div>
+            </div>
           </div>
-          <div className={style.BarcodeBox}></div>
+          <div className={style.BarcodeBox}>
+            <QRCode className={style.barcode} value={"test"} />
+          </div>
         </div>
       </div>
     </Fragment>
